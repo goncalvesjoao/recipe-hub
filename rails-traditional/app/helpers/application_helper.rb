217de7932438e9f030a43b18b45abe81cac4655a
Joao Goncalves <goncalves.joao@gmail.com>
2026-03-26 18:43:51 +0900

    added pico style

--- rails-traditional/app/helpers/application_helper.rb
+++ rails-traditional/app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def active_link_to(name, path, html_options = {})
+    active_class = current_page?(path) ? 'active' : ''
+    html_options[:class] = [html_options[:class], active_class].compact.join(' ')
+    link_to(name, path, html_options)
+  end
 end
