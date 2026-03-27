ddc5e874f235d01e21d6a1b192dbf75b9a81672e
Joao Goncalves <goncalves.joao@gmail.com>
2026-03-27 19:55:44 +0900

    turbo-stream success

--- rails-traditional/app/controllers/recipes/user_likes_controller.rb
+++ rails-traditional/app/controllers/recipes/user_likes_controller.rb
@@ -2,15 +2,6 @@ class Recipes::UserLikesController < Recipes::BaseController
   # POST /user_likes
   def create
     # @recipe_user_like = @recipe.recipe_user_likes.create!(user: Current.user)
-
-    respond_to do |format|
-      format.turbo_stream do
-        render turbo_stream: turbo_stream.replace("#{dom_id(@recipe)}-header", partial: "recipes/_recipe_header",
-          locals: { recipe: @recipe })
-      end
-
-      format.html { render "recipes/_recipe_header", locals: { recipe: @recipe } }
-    end
   end
 
   # DELETE /user_likes
