b643a5732558fbfdffe56ca03e8917137d53516d
Joao Goncalves <goncalves.joao@gmail.com>
2026-03-27 19:34:06 +0900

    added vs extensions for rails

--- rails-traditional/app/controllers/recipes/user_likes_controller.rb
+++ rails-traditional/app/controllers/recipes/user_likes_controller.rb
@@ -1,9 +1,16 @@
 class Recipes::UserLikesController < Recipes::BaseController
   # POST /user_likes
   def create
-    @recipe_user_like = @recipe.recipe_user_likes.create!(user: Current.user)
+    # @recipe_user_like = @recipe.recipe_user_likes.create!(user: Current.user)
 
-    render 'recipes/_recipe_header', locals: { recipe: @recipe }
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace("#{dom_id(@recipe)}-header", partial: "recipes/_recipe_header",
+          locals: { recipe: @recipe })
+      end
+
+      format.html { render "recipes/_recipe_header", locals: { recipe: @recipe } }
+    end
   end
 
   # DELETE /user_likes
@@ -11,6 +18,6 @@ class Recipes::UserLikesController < Recipes::BaseController
     @recipe_user_like = @recipe.recipe_user_likes.find_by!(user: Current.user)
     @recipe_user_like.destroy!
 
-    render 'recipes/_recipe_header', locals: { recipe: @recipe }
+    render "recipes/_recipe_header", locals: { recipe: @recipe }
   end
 end
